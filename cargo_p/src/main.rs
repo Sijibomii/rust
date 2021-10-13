4ae1d15371e119c2c856eabd594f7ac63667eb51
unknown <sijibomiolajubu@gmail.com>
2021-10-13 19:23:38 +0100

    loops

--- cargo_p/src/main.rs
+++ cargo_p/src/main.rs
@@ -1,3 +1,9 @@
+use std::convert::TryInto; 
+use num::complex::Complex;//num dep 
+//(::)=>namespace operator. I.E only the Complex type is imported
+use std::time::{Duration, Instant}; 
+
+ 
 fn main() {
     let a = 10;
     let b: i32 = 20;//i32 is the type-> 32 bit signed integer
@@ -39,8 +45,128 @@ fn main() {
     //a signed 32bit
     println!("Ten is less than one hundred.");
     }
-    }
+    //another method of casting different types.
+    let a: i32 = 10;
+    let b: u16 = 100;
+
+    let b_ = b.try_into().unwrap();
+
+    if a < b_ {
+     println!("Ten is less than one hundred.");
+     }
+    let abc: (f32, f32, f32) = (0.1, 0.2, 0.3);
+    let xyz: (f64, f64, f64) = (0.1, 0.2, 0.3);
+    println!("abc (f32)");
+    println!(" 0.1 + 0.2: {:x}", (abc.0 + abc.1).to_bits());
+    println!(" 0.3: {:x}", (abc.2).to_bits());
+    println!();
+ 
+    println!("xyz (f64)");
+    println!(" 0.1 + 0.2: {:x}", (xyz.0 + xyz.1).to_bits());
+    println!(" 0.3: {:x}", (xyz.2).to_bits());
+    println!();
+     //floating numbers should not be compared for equality bc of the way rust stores them
+     assert!(abc.0 + abc.1 == abc.2);//abc.0=>0.1, abc.1=>0.2 
+    // assert!(xyz.0 + xyz.1 == xyz.2);//this crashes the program
     
+
+    //this code checks for acceptable margin for equality between numbers
+    let result: f32 = 0.1 + 0.1;
+    let desired: f32 = 0.2;
+    let absolute_difference = (desired - result).abs();
+    assert!(absolute_difference <= f32::EPSILON);
+     
+    let x = (-42.0_f32).sqrt();//returns NaN
+    println!("{}",x)
+    //assert_eq!(x, x);//always crashes bc NaN are never equal
+
+    let a = Complex { re: 2.1, im: -1.2 };
+    let b = Complex::new(11.1, 22.2);
+    let result = a + b;
+    println!("{} + {}i", result.re, result.im)
+    
+    let container=[
+        45,
+        45,
+        67
+    ]
+    //for loops
+     //using a reference to the container makes sure the cointainer is still reachable after the loop ends.
+    for item in &container {
+        // ...
+    }
+    //&mut is used if you need to modify each item during the loop
+    for item in &mut container {
+        // ...
+       }
+
+    //after this loop ends, the container varible becomes invalid
+    for item in container {
+        // ...
+    }
+    //container can no longer be reached here.
+
+    //anonymus loops. when the item is not needed in the loop
+    for _ in 0..10 {
+        // ...
+       }
+    
+    //while loops
+    let mut samples = vec![];
+    while samples.len() < 10 {
+    let sample = take_sample();
+    if is_outlier(sample) {
+    continue;
+    }
+    samples.push(sample);
+    }
+        
+
+    let mut count = 0; //mut means the data should be changable
+    let time_limit = Duration::new(1,0);//creates a duration that lasts 1 second
+    let start = Instant::now();
+    while (Instant::now() - start) < time_limit {
+        count += 1;
+    }
+    println!("{}", count);
+
+    //loop continues running until it encounters a break keyword;
+    loop {
+        // ...
+    }
+    //A loop label is an identifier prefixed with an apostrophe (')
+    'outer: for x in 0.. {
+        for y in 0.. {
+        for z in 0.. {
+        if x + y + z > 100 {
+        break 'outer;
+        }
+        // ...
+        }
+        }
+    }
+    let n = 654321;
+    let description = match is_even(n) {
+        true => "even",
+        false => "odd",
+    };
+    println!("{} is {}", n, description);//prints odd since n is not even
+
+    //match is like if-else but it matches specific values
+    match item {
+        0 => {},//matches when item is exactly zero
+        10 ..= 20 => {},//matches between 10 and 20
+        40 | 80 => {},//matches 40 or 80
+        _ => {},//matches every other value
+    }
+
+    }
+    // fn is_outlier()-> bool{
+    //     return false
+    // }
+    // fn take_sample()-> i32{
+    //     40//some random number
+    // }
     fn add(i: i32, j: i32) -> i32 {
     //just like elixir, function returns last expression
     i + j
