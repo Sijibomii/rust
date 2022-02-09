371eca5eeac3b03c2194da28bd71ca1be5cdf6c4
unknown <sijibomiolajubu@gmail.com>
2022-02-09 02:03:53 +0100

    message passing

--- rusty/concurrency.rs
+++ rusty/concurrency.rs
@@ -1,4 +1,5 @@
 use std::{thread, time::Duration};
+use std::sync::mpsc;
 
 fn main(){
   let handle = thread::spawn(||{
@@ -14,4 +15,23 @@ fn main(){
   }
 
   handle.join().unwrap();// this waits for our spawned thread to finish executing
+
+  // Channels are ways in which threads in rust can pass messages to one another
+  // to create a new channel
+  //tx -> transmitter, rx -> receiver
+  let (tx, rx) = mpsc::channel();
+  // to send messages from more than one channel to the receiver(the main thread) we can clone the tx
+  let tx2 = tx.clone();
+  thread::spawn(move ||{
+    let message = String::from("msg1");
+    tx.send(message).unwrap();
+  });
+
+  thread::spawn(move ||{
+    let message = String::from("msg2");
+    tx2.send(message).unwrap();
+  });
+
+  let received = rx.recv().unwrap();
+  println!("{}", received);
 }
