41f262843e041f48b02863def5745257b2b6aea3
unknown <sijibomiolajubu@gmail.com>
2022-02-09 01:45:17 +0100

    creating threads

---
+++ rusty/concurrency.rs
@@ -0,0 +1,17 @@
+use std::{thread, time::Duration};
+
+fn main(){
+  let handle = thread::spawn(||{
+    for i in  1..10{
+      println!("{} from spawned thread", i);
+      thread::sleep(Duration::from_millis(1));
+    }
+  });
+
+  for i in  1..5{
+    println!("{} from main thread", i);
+    thread::sleep(Duration::from_millis(1));
+  }
+
+  handle.join().unwrap();// this waits for our spawned thread to finish executing
+}
